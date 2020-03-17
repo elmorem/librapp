66a535dd366acd7262d14ff40ea56ca4e5fe4035
elmorem <elmorem@gmail.com>
2020-03-17 08:59:27 -0700

    book model created

--- books/models.py
+++ books/models.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from books import db 
+
 
 class Book(db.Model):
-    id = db.co
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(128))
+    title = db.Column(db.String(256))
