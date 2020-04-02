31b32e6dcc5e98da9e54624f4d3fe5166d78f1c8
elmorem <elmorem@gmail.com>
2020-04-01 19:18:41 -0700

    comment

--- books/models.py
+++ books/models.py
@@ -3,26 +3,26 @@ from  import SQLAlchemy
 from books import db 
 
 
-class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    author = db.Column(db.String(128))
-    title = db.Column(db.String(256))
-    publisher= db.Column(db.String(128))
-    pub_location= db.Column(db.String(128))
-    pub_date= db.Column(db.Integer)
-    other=db.Column(db.String(256))
-    note=db.Column(db.TEXT)
+# class Book(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     author = db.Column(db.String(128))
+#     title = db.Column(db.String(256))
+#     publisher= db.Column(db.String(128))
+#     pub_location= db.Column(db.String(128))
+#     pub_date= db.Column(db.Integer)
+#     other=db.Column(db.String(256))
+#     note=db.Column(db.TEXT)
 
-    def __init__(self, author, title, publisher, location, date):
-        self.author=author
-        self.title=title
-        self.publisher=publisher
-        self.pub_location=location
-        self.pub_date=date   
+#     def __init__(self, author, title, publisher, location, date):
+#         self.author=author
+#         self.title=title
+#         self.publisher=publisher
+#         self.pub_location=location
+#         self.pub_date=date   
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    first_name= db.Column(db.String(128))
-    last_name= db.Column(db.String(128))
-    username= db.Column(db.String(128))
-    password= HASH
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     first_name= db.Column(db.String(128))
+#     last_name= db.Column(db.String(128))
+#     username= db.Column(db.String(128))
+#     password= HASH
