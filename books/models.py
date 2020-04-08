64b509557147bbc40aca217c82e854d017532cf5
elmorem <elmorem@gmail.com>
2020-04-08 13:25:10 -0700

    commit2

--- books/models.py
+++ books/models.py
@@ -27,4 +27,27 @@ class User(db.Model):
     username= db.Column(db.String(128))
     password= HASH
 
-    
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(128))
+    title = db.Column(db.String(256))
+    publisher= db.Column(db.String(128))
+    pub_location= db.Column(db.String(128))
+    pub_date= db.Column(db.Integer)
+    other=db.Column(db.String(256))
+    note=db.Column(db.TEXT)
+
+    def __init__(self, author, title, publisher, location, date):
+        self.author=author
+        self.title=title
+        self.publisher=publisher
+        self.pub_location=location
+        self.pub_date=date   
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name= db.Column(db.String(128))
+    last_name= db.Column(db.String(128))
+    username= db.Column(db.String(128))
+    password= HASH
