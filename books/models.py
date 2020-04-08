e5d986dffeeac462b7b96d8b97123ae1b285f67d
elmorem <elmorem@gmail.com>
2020-04-08 13:26:16 -0700

    commit1

--- books/models.py
+++ books/models.py
@@ -37,11 +37,3 @@ class User(db.Model):
 #     pub_date= db.Column(db.Integer)
 #     other=db.Column(db.String(256))
 #     note=db.Column(db.TEXT)
-
-#     def __init__(self, author, title, publisher, location, date):
-#         self.author=author
-#         self.title=title
-#         self.publisher=publisher
-#         self.pub_location=location
-#         self.pub_date=date   
-
